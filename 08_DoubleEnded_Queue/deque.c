187790e055c82fdf54402b2b95b751ffac4bd10c
GabrielZuany <gzuanydev@gmail.com>
2023-06-10 22:18:04 -0300

    Generic Deque implemented

--- 08_DoubleEnded_Queue/deque.c
+++ 08_DoubleEnded_Queue/deque.c
@@ -1,40 +1,93 @@
-#include "deque.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "../04_Double_Linked_List/node.h"
 
-struct Deque{
-    CircularVector *cv;
-};
+typedef struct Deque {
+    Node* front;
+    Node* rear;
+} Deque;
 
-Deque *deque_new(int define_total_capacity){
-    Deque *d = (Deque*)malloc(sizeof(Deque));
-    d->cv = dynamic_circular_vector_new(define_total_capacity);
-    return d;
+Deque* deque_create() {
+    Deque* deque = (Deque*)malloc(sizeof(Deque));
+    deque->front = NULL;
+    deque->rear = NULL;
+    return deque;
 }
 
-void deque_delete(Deque *d){
-    circular_vector_free(d->cv);
-    free(d);
+int deque_empty(Deque* deque) {
+    return deque->front == NULL;
 }
 
-void deque_push_front(Deque *d, int value){
-    circular_vector_insert_into(d->cv, 0, value);
+void deque_push_front(Deque* deque, void* data) {
+    Node* newNode = node_construct(data, deque->front, NULL);
+    if (deque_empty(deque)) {
+        deque->rear = newNode;
+    } else {
+        node_set_prev(deque->front, newNode);
+    }
+    deque->front = newNode;
 }
 
-void deque_push_back(Deque *d, int value){
-    circular_vector_push_back(d->cv, value);
+void deque_push_back(Deque* deque, void* data) {
+    Node* newNode = node_construct(data, NULL, deque->rear);
+    if (deque_empty(deque)) {
+        deque->front = newNode;
+    } else {
+        node_set_next(deque->rear, newNode);
+    }
+    deque->rear = newNode;
 }
 
-void deque_pop_front(Deque *d){
-    circular_vector_pop_front(d->cv);
+void deque_pop_front(Deque* deque) {
+    if (deque_empty(deque)) {
+        printf("Deque is empty.\n");
+        return;
+    }
+
+    Node* frontNode = deque->front;
+    deque->front = node_get_next(frontNode);
+
+    if (deque->front == NULL)
+        deque->rear = NULL;
+    else
+        node_set_prev(deque->front, NULL);
+
+    node_destroy(frontNode);
 }
 
-void deque_pop_back(Deque *d){
-    circular_vector_pop_back(d->cv);
+void deque_pop_back(Deque* deque) {
+    if (deque_empty(deque)) {
+        printf("Deque is empty.\n");
+        return;
+    }
+
+    Node* rearNode = deque->rear;
+    deque->rear = node_get_prev(rearNode);
+
+    if (deque->rear == NULL)
+        deque->front = NULL;
+    else
+        node_set_next(deque->rear, NULL);
+
+    node_destroy(rearNode);
 }
 
-int deque_get(Deque *d, int index){
-    return circular_vector_get(d->cv, index);
+void deque_print(Deque* deque, void (*printData)(void*)) {
+    if (deque_empty(deque)) {
+        printf("Deque is empty.\n");
+        return;
+    }
+
+    Node* current = deque->front;
+    while (current != NULL) {
+        printData(node_get_data(current));
+        current = node_get_next(current);
+    }
+    printf("\n");
 }
 
-void deque_print(Deque *d){
-    circular_vector_print(d->cv);
+void freeDeque(Deque* deque) {
+    while (!deque_empty(deque))
+        deque_pop_front(deque);
+    free(deque);
 }
--- 08_DoubleEnded_Queue/deque.h
+++ 08_DoubleEnded_Queue/deque.h
@@ -1,26 +1,95 @@
 #ifndef _DEQUE_H_
 #define _DEQUE_H_
-#include "circular_vector.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
 
-typedef struct Deque Deque;
+#include "node.h"
 
-Deque *deque_new(int define_total_capacity);
+/**
+ * @file deque.h
+ * @brief Double-Ended Queue (Deque) Data Structure
+ */
 
-void deque_delete(Deque *d);
+/**
+ * @brief Deque structure
+ */
+typedef struct Deque {
+    Node* front; /**< Pointer to the front node of the deque */
+    Node* rear; /**< Pointer to the rear node of the deque */
+} Deque;
 
-void deque_push_front(Deque *d, int value);
+/**
+ * @brief Create an empty deque
+ *
+ * @return Pointer to the newly created deque
+ *
+ * @note Complexity: O(1)
+ */
+Deque* deque_create();
 
-void deque_push_back(Deque *d, int value);
+/**
+ * @brief Check if the deque is empty
+ *
+ * @param deque Pointer to the deque
+ * @return 1 if the deque is empty, 0 otherwise
+ *
+ * @note Complexity: O(1)
+ */
+int deque_empty(Deque* deque);
 
-void deque_pop_front(Deque *d);
+/**
+ * @brief Insert an element at the front of the deque
+ *
+ * @param deque Pointer to the deque
+ * @param data Pointer to the data to be inserted
+ *
+ * @note Complexity: O(1)
+ */
+void deque_push_front(Deque* deque, void* data);
 
-void deque_pop_back(Deque *d);
+/**
+ * @brief Insert an element at the rear of the deque
+ *
+ * @param deque Pointer to the deque
+ * @param data Pointer to the data to be inserted
+ *
+ * @note Complexity: O(1)
+ */
+void deque_push_back(Deque* deque, void* data);
 
-int deque_get(Deque *d, int index);
+/**
+ * @brief Delete an element from the front of the deque
+ *
+ * @param deque Pointer to the deque
+ *
+ * @note Complexity: O(1)
+ */
+void deque_pop_front(Deque* deque);
 
-void deque_print(Deque *d);
+/**
+ * @brief Delete an element from the rear of the deque
+ *
+ * @param deque Pointer to the deque
+ *
+ * @note Complexity: O(1)
+ */
+void deque_pop_back(Deque* deque);
+
+/**
+ * @brief Display the elements in the deque
+ *
+ * @param deque Pointer to the deque
+ * @param printData Function pointer to print the data stored in each node
+ *
+ * @note Complexity: O(n)
+ */
+void deque_print(Deque* deque, void (*printData)(void*));
+
+/**
+ * @brief Free memory occupied by the deque
+ *
+ * @param deque Pointer to the deque
+ *
+ * @note Complexity: O(n)
+ */
+void freeDeque(Deque* deque);
 
 #endif
