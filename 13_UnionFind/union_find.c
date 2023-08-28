813f0dae9d0dcaac664107be96f4db36018e7bed
GabrielZuany <gzuanydev@gmail.com>
2023-08-28 12:36:09 -0300

    New features

--- 13_UnionFind/union_find.c
+++ 13_UnionFind/union_find.c
@@ -43,26 +43,92 @@ void uf_destroy(union_find* uf){
     free(uf);
 }
 
-int _root_index(union_find* uf, void* node){
-    int i = 0;
-    while (!(uf->compare(uf->tree_nodes[i].data, node))) // while not reach the root
+int _root_index(union_find* uf, int i){
+    while (!(uf->tree_nodes[i]._priority == i)) // while not reach the root
     {
-        uf->tree_nodes[i] = uf->tree_nodes[uf->tree_nodes[i]._priority]; // copy?
+        uf->tree_nodes[i]._priority = uf->tree_nodes[uf->tree_nodes[i]._priority]._priority;
         i = uf->tree_nodes[i]._priority;
     }
     return i;
 }
 
-void* uf_find(union_find* uf, void* node){
-
+void* uf_find_data(union_find* uf, tree_node* node){
+    return uf->tree_nodes[_root_index(uf, node->_priority)].data;
 }
 
-void uf_union(union_find* uf, void* node1, void* node2){
-    tree_node n1 = uf->tree_nodes[_root_index(uf, node1)];
-    tree_node n2 = uf->tree_nodes[_root_index(uf, node2)];
-    
+tree_node* uf_find_node(union_find* uf, int priority){
+    return &(uf->tree_nodes[priority]);
 }
 
-bool uf_is_connected(union_find* uf, void* node1, void* node2){
-
+int _tree_node_is_equal(union_find* uf, tree_node n1, tree_node n2){
+    return (n1._priority == n2._priority && uf->compare(n1.data, n2.data));
 }
+
+tree_node* uf_create_node(union_find* uf, void* new_node, int priority){
+    uf->tree_nodes[priority].data = new_node;
+    uf->tree_nodes[priority]._priority = priority;
+    tree_node* tn = &(uf->tree_nodes[priority]);
+    return tn;
+}
+
+void uf_union(union_find* uf, tree_node* node1, tree_node* node2){
+    int n1_idx = _root_index(uf, node1->_priority);
+    int n2_idx = _root_index(uf, node2->_priority);
+    tree_node n1 = uf->tree_nodes[n1_idx];
+    tree_node n2 = uf->tree_nodes[n2_idx];
+    if(_tree_node_is_equal(uf, n1, n2)){
+        return;
+    }
+    if(uf->size_trees_arr[n1_idx] < uf->size_trees_arr[n2_idx]){
+        uf->tree_nodes[n1_idx]._priority = uf->tree_nodes[n2_idx]._priority;
+        uf->size_trees_arr[n2_idx] += uf->size_trees_arr[n1_idx];
+    }else{
+        uf->tree_nodes[n2_idx]._priority = uf->tree_nodes[n1_idx]._priority;
+        uf->size_trees_arr[n1_idx] += uf->size_trees_arr[n2_idx];
+    }
+}
+
+int uf_is_connected(union_find* uf, tree_node* node1, tree_node* node2){
+    int n1_idx = _root_index(uf, node1->_priority);
+    int n2_idx = _root_index(uf, node2->_priority);
+    tree_node n1 = uf->tree_nodes[n1_idx];
+    tree_node n2 = uf->tree_nodes[n2_idx];
+    return n1._priority == n2._priority;
+}
+
+#ifdef _DEV_
+void _d_uf_print_(union_find* uf){
+    for(int i = 0; i < uf->size; i++){
+        printf("%d ", i);
+    }
+    printf(" => ARR IDX\n");
+    for(int i = 0; i < uf->size; i++){
+        printf("| ");
+    }
+    printf("\n");
+    for(int i = 0; i < uf->size; i++){
+        printf("v ");
+    }
+    printf("\n");
+    for(int i = 0; i < uf->size; i++){
+        printf("%d ", uf->tree_nodes[i]._priority);
+    }
+    printf(" => PRIORITY\n");
+}
+
+tree_node** _d_uf_fill_int_(union_find* uf){
+    int size = uf->size;
+    int** arr = malloc(sizeof(int*) * size);
+    for(int i = 0; i < size; i++){
+        arr[i] = malloc(sizeof(int));
+        *arr[i] = i;
+    }
+
+    tree_node** nodes = malloc(sizeof(tree_node*) * size);
+    for(int i = 0; i < size; i++){
+        nodes[i] = uf_create_node(uf, arr[i], i);
+    }
+    return nodes;
+}
+
+#endif
--- 13_UnionFind/union_find.h
+++ 13_UnionFind/union_find.h
@@ -27,10 +27,85 @@ union_find* uf_init(int size, uf_compare_fn compare_fn, uf_destroy_fn destroy_fn
  */
 void uf_destroy(union_find* uf);
 
-void* uf_find(union_find* uf, void* node);
+/**
+ * @brief Retrieves the data stored into tree_node datatype 
+ * 
+ * @param uf 
+ * @param priority 
+ * @return tree_node* 
+ * 
+ * @note Complexity: O(log(n))
+ */
+void* uf_find_data(union_find* uf, tree_node* node);
 
-void uf_union(union_find* uf, void* node1, void* node2);
+/**
+ * @brief Retrieves the node based in given priority value;
+ * 
+ * @param uf 
+ * @param priority 
+ * @return tree_node* 
+ *  
+ *  @note Complexity: O(1)
+ */
+tree_node* uf_find_node(union_find* uf, int priority);
 
-bool uf_is_connected(union_find* uf, void* node1, void* node2);
+/**
+ * @brief Join 2 nodes using weighted quick sort
+ * 
+ * @param uf 
+ * @param node1 
+ * @param node2 
+ * 
+ * @note Complexity: O(log(n))
+ */
+void uf_union(union_find* uf, tree_node* node1, tree_node* node2);
+
+/**
+ * @brief 
+ * 
+ * @param uf 
+ * @param node1 
+ * @param node2 
+ * @return int 
+ * 
+ * @note Complexity: O(log(n))
+ */
+int uf_is_connected(union_find* uf, tree_node* node1, tree_node* node2);
+
+/**
+ * @brief Assigns a void* data value into a node previously allocacted
+ * 
+ * @param uf 
+ * @param new_node void* generic value
+ * @param priority priority (array position) in 'tree'.
+ * @return tree_node* 
+ * 
+ * @note Complexity: O(1)
+ */
+tree_node* uf_create_node(union_find* uf, void* new_node, int priority);
+
+#ifdef _DEV_
+    /**
+     * @brief Prints the union find data structure (for debug/test purposes -> compile with $ args -D _DEV_)
+     * 
+     * @note Bind function
+     * 
+     * @param uf 
+     * 
+     * @note Complexity: O(n)
+     */
+    void _d_uf_print_(union_find* uf);
+
+    /**
+     * @brief Fills the union find data structure with integers (for debug/test purposes -> compile with $ args -D _DEV_)
+     * 
+     * @note Bind function
+     * 
+     * @param uf 
+     * 
+     * @note Complexity: O(n)
+     */
+    tree_node** _d_uf_fill_int_(union_find* uf);
+#endif
 
 #endif
